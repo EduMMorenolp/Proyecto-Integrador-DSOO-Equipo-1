67e49beb93d69adc7d4dff5777e6b156ba7efa2c
Lautaro Colella <lautarocolella1@gmail.com>
2025-05-26 23:03:40 -0300

    Frontend form carnet

--- ClubDeportivo/Controllers/Forms/PantallaPrincipal/FrmEntregarCarnet.cs
+++ ClubDeportivo/Controllers/Forms/PantallaPrincipal/FrmEntregarCarnet.cs
@@ -16,5 +16,24 @@ namespace ClubDeportivo.Controllers.Forms.PantallaPrincipal
         {
             InitializeComponent();
         }
+
+        private void FrmEntregarCarnet_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // TO DO Backend:
+        // Cuando el usuario le da click al boton limpiar campo:
+        // - Vaciar el campo
+
+        // Cuando el usuario le da click al boton salir:
+        // - Cerrar formulario
+
+        // Cuando el usuario le da click al boton enviar:
+        // - Validar que el valor del campo sea un numero y que sea menor a 10 digitos
+        // - Verificar que el socio existe en la tabla socios, usando el DNI
+        // - Verificar que el socio tenga el valor de tieneCarnet en falso
+        // - Cambiar el valor por verdadero
+        // - Mostrar los mensajes de exito o error en la label lblOutput
     }
 }
