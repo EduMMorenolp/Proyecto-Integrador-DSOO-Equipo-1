7eea9992e5199f0291395c9e1a8761203bba954d
Lautaro Colella <lautarocolella1@gmail.com>
2025-05-26 23:25:14 -0300

    Frontend form cuotas vencidas

--- ClubDeportivo/Controllers/Forms/PantallaPrincipal/FrmListarCuotasVencidas.Designer.cs
+++ ClubDeportivo/Controllers/Forms/PantallaPrincipal/FrmListarCuotasVencidas.Designer.cs
@@ -28,18 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            gbDniSocio = new GroupBox();
+            txtDni = new TextBox();
+            lblDNI = new Label();
+            btnFiltrar = new Button();
+            btnVerTodas = new Button();
+            btnVerVencidas = new Button();
+            btnVerNoVencidas = new Button();
+            gbDniSocio.SuspendLayout();
             SuspendLayout();
+            // 
+            // gbDniSocio
+            // 
+            gbDniSocio.Controls.Add(txtDni);
+            gbDniSocio.Controls.Add(lblDNI);
+            gbDniSocio.Controls.Add(btnFiltrar);
+            gbDniSocio.Location = new Point(285, 37);
+            gbDniSocio.Name = "gbDniSocio";
+            gbDniSocio.Size = new Size(221, 120);
+            gbDniSocio.TabIndex = 2;
+            gbDniSocio.TabStop = false;
+            gbDniSocio.Text = "Filtrar por documento del socio";
+            // 
+            // txtDni
+            // 
+            txtDni.Location = new Point(59, 33);
+            txtDni.Name = "txtDni";
+            txtDni.Size = new Size(140, 23);
+            txtDni.TabIndex = 1;
+            // 
+            // lblDNI
+            // 
+            lblDNI.AutoSize = true;
+            lblDNI.Location = new Point(23, 36);
+            lblDNI.Name = "lblDNI";
+            lblDNI.Size = new Size(30, 15);
+            lblDNI.TabIndex = 2;
+            lblDNI.Text = "DNI:";
+            // 
+            // btnFiltrar
+            // 
+            btnFiltrar.Location = new Point(76, 78);
+            btnFiltrar.Name = "btnFiltrar";
+            btnFiltrar.Size = new Size(75, 23);
+            btnFiltrar.TabIndex = 4;
+            btnFiltrar.Text = "Filtrar";
+            btnFiltrar.UseVisualStyleBackColor = true;
+            // 
+            // btnVerTodas
+            // 
+            btnVerTodas.Location = new Point(361, 190);
+            btnVerTodas.Name = "btnVerTodas";
+            btnVerTodas.Size = new Size(75, 23);
+            btnVerTodas.TabIndex = 5;
+            btnVerTodas.Text = "Ver todas";
+            btnVerTodas.UseVisualStyleBackColor = true;
+            // 
+            // btnVerVencidas
+            // 
+            btnVerVencidas.Location = new Point(128, 190);
+            btnVerVencidas.Name = "btnVerVencidas";
+            btnVerVencidas.Size = new Size(94, 23);
+            btnVerVencidas.TabIndex = 6;
+            btnVerVencidas.Text = "Ver vencidas";
+            btnVerVencidas.UseVisualStyleBackColor = true;
+            // 
+            // btnVerNoVencidas
+            // 
+            btnVerNoVencidas.Location = new Point(577, 190);
+            btnVerNoVencidas.Name = "btnVerNoVencidas";
+            btnVerNoVencidas.Size = new Size(110, 23);
+            btnVerNoVencidas.TabIndex = 7;
+            btnVerNoVencidas.Text = "Ver no vencidas";
+            btnVerNoVencidas.UseVisualStyleBackColor = true;
             // 
             // FrmListarCuotasVencidas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnVerNoVencidas);
+            Controls.Add(btnVerVencidas);
+            Controls.Add(btnVerTodas);
+            Controls.Add(gbDniSocio);
             Name = "FrmListarCuotasVencidas";
             Text = "Lista Cuotas Vencidas";
+            gbDniSocio.ResumeLayout(false);
+            gbDniSocio.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox gbDniSocio;
+        private TextBox txtDni;
+        private Label lblDNI;
+        private Button btnFiltrar;
+        private Button btnVerTodas;
+        private Button btnVerVencidas;
+        private Button btnVerNoVencidas;
     }
 }
--- ClubDeportivo/Controllers/Forms/PantallaPrincipal/FrmListarCuotasVencidas.cs
+++ ClubDeportivo/Controllers/Forms/PantallaPrincipal/FrmListarCuotasVencidas.cs
@@ -16,5 +16,23 @@ namespace ClubDeportivo.Controllers.Forms.PantallaPrincipal
         {
             InitializeComponent();
         }
+
+        // TO DO:
+        // Al hacer click en filtrar:
+        // - Validar que el valor del campo sea un numero y que sea menor a 10 digitos
+        // - Verificar que el socio existe
+        // - Crear un groupBox dinamicamente con el DNI del usuario y si la cuota esta vencida o no
+
+        // Al hacer click en ver vencidas:
+        // - Obtener de la base de datos el DNI de los socios cuya cuota esta vencida
+        // - Crear un groupBox dinamicamente por cada socio donde se vea el DNI y que la cuota esta vencida
+
+        // Al hacer click en ver no vencidas:
+        // - Obtener de la base de datos el DNI de los socios cuya cuota NO esta vencida
+        // - Crear un groupBox dinamicamente por cada socio donde se vea el DNI y que la cuota NO esta vencida
+
+        // Al hacer click en ver todas:
+        // - Obtener de la base de datos el DNI de todos los socios
+        // - Crear un groupBox dinamicamente por cada socio donde se vea el DNI y si la cuota esta o no vencida
     }
 }
